5ae8f4f8a129f779fdd91815ca9a62c88d43db43
chao-ji <jic@indiana.edu>
2018-04-18 20:01:35 -0700

    Update and rename model.py to pascal/model.py

--- pascal/model.py
+++ pascal/model.py
@@ -18,6 +18,8 @@ VGG16_CONV_LAYERS = (("conv1_1", "conv1_2"),
 UPSAMPLE_FACTORS = 32, 16, 8
 
 TRAIN = tf.contrib.learn.ModeKeys.TRAIN
+INFER = tf.contrib.learn.ModeKeys.INFER
+
 
 class FCNVGGModel(object):
   def __init__(self, hparams, dataset, mode=None):
@@ -27,8 +29,7 @@ class FCNVGGModel(object):
     self._upsample_factor = hparams.upsample_factor
     self._mode = mode
     self._weights = collections.OrderedDict()
-    (self._logits, self._loss, self._predictions
-        ) = self._build_graph(hparams, dataset)
+    self._logits, self._predictions = self._build_graph(hparams, dataset)
 
   @property
   def upsample_factor(self):
@@ -43,10 +44,6 @@ class FCNVGGModel(object):
     return self._logits
 
   @property
-  def loss(self):
-    return self._loss
-
-  @property
   def predictions(self):
     return self._predictions
 
@@ -57,7 +54,7 @@ class FCNVGGModel(object):
   def _build_graph(self, hparams, dataset):
     ftmps = images = dataset.images
     num_classes = hparams.num_classes
-    init_weights = _get_init_weights(self.upsample_factor, hparams)
+    init_weights = _get_init_weights(hparams)
     weights = self.weights
 
     layers = collections.OrderedDict() 
@@ -71,7 +68,7 @@ class FCNVGGModel(object):
       ftmps = tf.nn.max_pool(ftmps, ksize=(1, 2, 2, 1), strides=(1, 2, 2, 1),
           padding="SAME", name=name)
       layers[name] = ftmps
-#      print(name + "\t" + str(ftmps.shape))
+      print(name + "\t" + str(ftmps.shape))
 
     fc6 = fc2conv_layer(ftmps,
                         hparams=hparams,
@@ -89,19 +86,12 @@ class FCNVGGModel(object):
                         new_shape=(1, 1, 4096, 4096),
                         init_weights=init_weights)
 
-    if self.upsample_factor == 32:
-      out = project_layer(fc7,
-                          hparams=hparams,
-                          weights=weights,
-                          out_depth=num_classes,
-                          name="out")
-    else:
-      out = project_layer(fc7,
-                          hparams=hparams,
-                          weights=weights,
-                          out_depth=num_classes,
-                          name="out",
-                          init_weights=init_weights)
+    out = project_layer(fc7,
+                        hparams=hparams,
+                        weights=weights,
+                        out_depth=num_classes,
+                        name="out",
+                        init_weights=init_weights)
 
     logits_shape = tf.concat([tf.shape(images)[:3], [num_classes]], axis=0)
 
@@ -115,72 +105,37 @@ class FCNVGGModel(object):
       logits = _fcn8s_logits(out,
           hparams, weights, layers, num_classes, logits_shape, init_weights)
 
-    if self.mode != tf.contrib.learn.ModeKeys.INFER:
-      loss = _compute_loss(logits, dataset.labels)
-    else:
-      loss = tf.no_op()
-
-    predictions = _compute_predictions(logits)
-    return logits, loss, predictions
+    predictions = tf.argmax(logits, axis=3, output_type=tf.int32) 
+    return logits, predictions
 
 
-def _get_init_weights(upsample_factor, hparams):
-  if upsample_factor == 32:
+def _get_init_weights(hparams):
+  if hparams.upsample_factor == 32:
     path = hparams.vgg16_npy_path
-  elif upsample_factor == 16:
+  elif hparams.upsample_factor == 16:
     path = hparams.fcn_32s_npy_path
-  elif upsample_factor == 8:
-    path = hparams.fcn_16s_npy_path
+  elif hparams.upsample_factor == 8:
+    if hparams.fcn8s_oneshot:
+      path = hparams.vgg16_npy_path
+    else:
+      path = hparams.fcn_16s_npy_path
   init_weights = np.load(path).item()
   return init_weights
 
 
-def _compute_loss(logits, labels):
-  loss = tf.reduce_mean(
-      tf.nn.sparse_softmax_cross_entropy_with_logits(
-          logits=logits, labels=tf.squeeze(labels, squeeze_dims=[3])))
-  return loss
-
-
-def _compute_predictions(logits):
-  return tf.expand_dims(
-      tf.argmax(logits, axis=3, output_type=tf.int32), axis=3)
-
-
 def _fcn32s_logits(
-    ftmps, hparams, weights, layers, num_classes, logits_shape):
+    ftmps, hparams, weights, layers, num_classes, logits_shape, init_weights):
   out_up32x = upsample_layer(ftmps,
+                             hparams,
                              weights=weights,
                              upsample_factor=32,
                              output_shape=logits_shape,
-                             name="out_up32x")
+                             name="out_up32x",
+                             init_weights=init_weights)
   return out_up32x
 
 
 def _fcn16s_logits(
-    ftmps, hparams, weights, layers, num_classes, logits_shape):
-  pool4_proj = project_layer(layers["pool4"],
-                             hparams=hparams,
-                             weights=weights,
-                             out_depth=num_classes,
-                             name="pool4_proj")
-  out_up2x = upsample_layer(ftmps,
-                            weights=weights,
-                            upsample_factor=2,
-                            output_shape=tf.shape(pool4_proj),
-                            name="out_up2x")
-
-  fused_pool4 = tf.add(pool4_proj, out_up2x, "fused_pool4")
-
-  fused_pool4_up16x = upsample_layer(fused_pool4,
-                                     weights=weights,
-                                     upsample_factor=16,
-                                     output_shape=logits_shape,
-                                     name="fused_pool4_up16x")
-  return fused_pool4_up16x
-
-
-def _fcn8s_logits(
     ftmps, hparams, weights, layers, num_classes, logits_shape, init_weights):
   pool4_proj = project_layer(layers["pool4"],
                              hparams=hparams,
@@ -188,13 +143,50 @@ def _fcn8s_logits(
                              out_depth=num_classes,
                              name="pool4_proj",
                              init_weights=init_weights)
-  pool3_proj = project_layer(layers["pool3"],
+  out_up2x = upsample_layer(ftmps,
+                            hparams,
+                            weights=weights,
+                            upsample_factor=2,
+                            output_shape=tf.shape(pool4_proj),
+                            name="out_up2x",
+                            init_weights=init_weights)
+
+  fused_pool4 = tf.add(pool4_proj, out_up2x, "fused_pool4")
+
+  fused_pool4_up16x = upsample_layer(fused_pool4,
+                                     hparams,
+                                     weights=weights,
+                                     upsample_factor=16,
+                                     output_shape=logits_shape,
+                                     name="fused_pool4_up16x",
+                                     init_weights=init_weights)
+  return fused_pool4_up16x
+
+
+def _fcn8s_logits(
+    ftmps, hparams, weights, layers, num_classes, logits_shape, init_weights):
+  pool4 = layers["pool4"]
+  if hparams.fcn8s_oneshot == True:
+    pool4 = tf.multiply(pool4, 0.01)
+  pool4_proj = project_layer(pool4,
                              hparams=hparams,
                              weights=weights,
                              out_depth=num_classes,
-                             name="pool3_proj")
+                             name="pool4_proj",
+                             init_weights=init_weights)
+
+  pool3 = layers["pool3"]
+  if hparams.fcn8s_oneshot == True:
+    pool3 = tf.multiply(pool3, 0.0001)
+  pool3_proj = project_layer(pool3,
+                             hparams=hparams,
+                             weights=weights,
+                             out_depth=num_classes,
+                             name="pool3_proj",
+                             init_weights=init_weights)
 
   out_up2x = upsample_layer(ftmps,
+                            hparams,
                             weights=weights,
                             upsample_factor=2,
                             output_shape=tf.shape(pool4_proj),
@@ -204,16 +196,20 @@ def _fcn8s_logits(
   fused_pool4 = tf.add(pool4_proj, out_up2x, "fused_pool4")
 
   fused_pool4_up2x = upsample_layer(fused_pool4,
+                                    hparams,
                                     weights=weights,
                                     upsample_factor=2,
                                     output_shape=tf.shape(pool3_proj),
-                                    name="fused_pool4_up2x")
+                                    name="fused_pool4_up2x",
+                                    init_weights=init_weights)
 
   fused_pool3 = tf.add(pool3_proj, fused_pool4_up2x, "fused_pool3")
 
   fused_pool3_up8x = upsample_layer(fused_pool3,
+                                    hparams,
                                     weights=weights,
                                     upsample_factor=8,
                                     output_shape=logits_shape,
-                                    name="fused_pool3_up8x")
+                                    name="fused_pool3_up8x",
+                                    init_weights=init_weights)
   return fused_pool3_up8x
